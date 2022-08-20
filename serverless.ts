2a1a4b6dd6e4154d2c1a881b404a637dd637ad51
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-08-20 11:26:28 -0300

    finalizando integração com o S3

--- serverless.ts
+++ serverless.ts
@@ -20,6 +20,11 @@ const serverlessConfiguration: AWS = {
         Effect: 'Allow',
         Action: ['dynamodb:*'],
         Resource: ['*'],
+      },
+      {
+        Effect: 'Allow',
+        Action: ['s3:*'],
+        Resource: ['*'],
       }
     ],
   },
--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -4,6 +4,7 @@ import { join } from "path"
 import { readFileSync } from "fs"
 import dayjs from "dayjs"
 import chromium from "chrome-aws-lambda"
+import { S3 } from "aws-sdk"
 
 import { document } from "../utils/dynamodbClient"
 
@@ -32,11 +33,6 @@ const compile = async (data: ITemplate) => {
 export const handler: APIGatewayProxyHandler = async (event) => {
   const { id, name, grade } = JSON.parse(event.body) as ICreateCertificate
 
-  await document.put({
-    TableName: "users_certificate",
-    Item: { id, name, grade, created_at: new Date().getTime() }
-  }).promise()
-
   const response = await document.query({
     TableName: "users_certificate",
     KeyConditionExpression: "id = :id",
@@ -44,6 +40,15 @@ export const handler: APIGatewayProxyHandler = async (event) => {
       ":id": id
     }
   }).promise()
+
+  const userAlreadyHasCertificate = response.Items[0]
+
+  if (!userAlreadyHasCertificate) {
+    await document.put({
+      TableName: "users_certificate",
+      Item: { id, name, grade, created_at: new Date().getTime() }
+    }).promise()
+  }
 
   const medalPath = join(process.cwd(), "src", "template", "selo.png")
   const medal = readFileSync(medalPath, "base64")
@@ -78,8 +83,21 @@ export const handler: APIGatewayProxyHandler = async (event) => {
 
   await browser.close()
 
+  const s3 = new S3()
+
+  await s3.putObject({
+    Bucket: "certificates-rafael-manfrim-ignite",
+    ContentType: "application/pdf",
+    Key: `${id}.pdf`,
+    ACL: "public-read",
+    Body: pdf
+  }).promise()
+
   return {
     statusCode: 201,
-    body: JSON.stringify(response.Items[0])
+    body: JSON.stringify({
+      message: "Certificado criado com sucesso",
+      url: `https://certificates-rafael-manfrim-ignite.s3.sa-east-1.amazonaws.com/${id}.pdf`
+    })
   }
 }
