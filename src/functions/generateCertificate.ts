a16fdee89af0056abc4ff1970fc2b858f55a3cbf
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-08-19 21:55:56 -0300

    implementando template de certificado

--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -1,10 +1,31 @@
 import { APIGatewayProxyHandler } from "aws-lambda"
+import * as handlebars from "handlebars"
+import { join } from "path"
+import { readFileSync } from "fs"
+import * as dayjs from "dayjs"
+
 import { document } from "../utils/dynamodbClient"
 
 interface ICreateCertificate {
   name: string
   id: string
   grade: string
+}
+
+interface ITemplate {
+  name: string
+  id: string
+  grade: string
+  medal: string
+  date: string
+}
+
+const compile = async (data: ITemplate) => {
+  const filePath = join(process.cwd(), "src", "template", "certificate.hbs")
+
+  const html = readFileSync(filePath, "utf-8")
+
+  return handlebars.compile(html)(data)
 }
 
 export const handler: APIGatewayProxyHandler = async (event) => {
@@ -22,6 +43,19 @@ export const handler: APIGatewayProxyHandler = async (event) => {
       ":id": id
     }
   }).promise()
+
+  const medalPath = join(process.cwd(), "src", "template", "selo.png")
+  const medal = readFileSync(medalPath, "base64")
+
+  const data: ITemplate = {
+    name,
+    id,
+    grade,
+    medal,
+    date: dayjs().format("DD/MM/YYYY")
+  }
+
+  const content = await compile(data)
 
   return {
     statusCode: 201,
